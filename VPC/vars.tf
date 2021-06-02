9a57c0fc8be76459ae3160532c9e833247044ae6
Adithyavardhan Garapati <agarapati@altimetrik.com>
2021-06-02 22:24:59 +0530

    added files

--- VPC/vars.tf
+++ VPC/vars.tf
@@ -2,11 +2,4 @@ variable "REGION" {
    type = string
    default = "us-east-1"
 }
-variable "ACCESS_KEY" {
-   type = string
-   default = "AKIATO4HIGKBJFHVNHDL"
-}
-variable "SECRET_KEY" {
-   type = string
-   default = "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"
-}
+
