b4524d0331273638dc9b1d6425cfee79719edd2e
Adithyavardhan Garapati <agarapati@altimetrik.com>
2021-06-04 15:58:16 +0530

    added files

--- VPC/modules/create_securitygroup/main.tf
+++ VPC/modules/create_securitygroup/main.tf
@@ -6,11 +6,14 @@ resource "aws_security_group" "security_group_prod" {
       protocol = -1
       cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-      from_port = 0
-      to_port = 0
-      protocol = -1
-      cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+      for_each = var.security_group_prod_ports
+      content {
+        from_port   = ingress.value
+        to_port     = ingress.value
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
   }
   tags = {
     "Name" = var.security_group_prod_name
--- VPC/modules/create_securitygroup/vars.tf
+++ VPC/modules/create_securitygroup/vars.tf
@@ -4,3 +4,8 @@ variable "vpc_id" {
 variable "security_group_prod_name" {
   default = "sg-prod"
 }
+variable "security_group_prod_ports" {
+  type        = list(number)
+  description = "list of ingress ports"
+  default     = [80, 8080, 443, 22]
+}
