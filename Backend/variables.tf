651a760aae677d158404bc699fe3fa6657beee48
Adithyavardhan Garapati <agarapati@altimetrik.com>
2021-06-10 17:50:51 +0530

    added files

--- Backend/variables.tf
+++ Backend/variables.tf
@@ -2,7 +2,7 @@ variable "s3_bucket_for_remotestate_name" {
     default = "s3-bucket-for-remote-state"
 }
 variable "region" {
-    default = "us-east-2"
+    default = "us-east-1"
 }
 variable "dynamodb_table_for_remotestate_name" {
     default = "dynamobd_state"  
